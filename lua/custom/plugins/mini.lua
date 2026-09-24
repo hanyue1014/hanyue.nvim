248a820e0c2f418090fa60a25b9adb0ed87d173e
hanyue1014 <wongxuanbin@gmail.com>
2026-09-24 17:17:55 +0800

    Add dashboard
    Add dashboard based on mini.nvim

--- lua/custom/plugins/mini.lua
+++ lua/custom/plugins/mini.lua
@@ -2,7 +2,7 @@ local gh = require('config.pack').gh
 
 -- ============================================================
 -- MINI.NVIM
--- icons, around/inside textobjects, surround, statusline
+-- icons, around/inside textobjects, surround, statusline, start screen
 --
 -- All the mini modules share one plugin, so they share one file.
 -- ============================================================
@@ -52,6 +52,74 @@ statusline.setup { use_icons = vim.g.have_nerd_font }
 -- cursor location to LINE:COLUMN
 ---@diagnostic disable-next-line: duplicate-set-field
 statusline.section_location = function() return '%2l:%-2v' end
+
+-- Start screen. Takes over the empty startup buffer and wipes it once you
+-- open something, so no [No Name] is left in the buffer list.
+--
+-- Type the first letters of an item to narrow it down, <CR> to run it.
+-- That's why the item names are plain text, the icons live in the section
+-- names and the bullet instead, where they don't get in the way of typing.
+local starter = require 'mini.starter'
+local icon = function(glyph) return vim.g.have_nerd_font and (glyph .. ' ') or '' end
+
+local header = function()
+  local hour = tonumber(os.date '%H')
+  local greeting = hour < 12 and 'Good morning' or hour < 18 and 'Good afternoon' or 'Good evening'
+  return table.concat({
+    '███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗',
+    '████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║',
+    '██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║',
+    '██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║',
+    '██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║',
+    '╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝',
+    '',
+    greeting .. ', welcome back.',
+  }, '\n')
+end
+
+local footer = function()
+  local v = vim.version()
+  return table.concat({
+    icon '\u{f073}' .. os.date '%A, %d %B %Y',
+    icon '\u{f1e6}' .. #vim.pack.get() .. ' plugins',
+    icon '\u{e62b}' .. string.format('v%d.%d.%d', v.major, v.minor, v.patch),
+  }, '   ')
+end
+
+-- Telescope is required lazily, it loads after this file
+local telescope = function(picker, opts)
+  return function() require('telescope.builtin')[picker](opts) end
+end
+
+-- Recent files in the cwd, renamed from the default
+-- "Recent files (current directory)" so it gets an icon like Actions
+local recent_files = function()
+  local items = starter.sections.recent_files(6, true, false)()
+  for _, item in ipairs(items) do
+    item.section = icon '\u{f1da}' .. 'Recent files'
+  end
+  return items
+end
+
+local actions = icon '\u{f0e7}' .. 'Actions'
+starter.setup {
+  header = header,
+  footer = footer,
+  items = {
+    { section = actions, name = 'Find file', action = telescope 'find_files' },
+    { section = actions, name = 'Grep text', action = telescope 'live_grep' },
+    { section = actions, name = 'Explorer', action = 'Neotree' },
+    { section = actions, name = 'New file', action = 'enew' },
+    { section = actions, name = 'Config', action = telescope('find_files', { cwd = vim.fn.stdpath 'config', follow = true }) },
+    { section = actions, name = 'Update plugins', action = function() vim.pack.update() end },
+    { section = actions, name = 'Quit', action = 'qall' },
+    recent_files,
+  },
+  content_hooks = {
+    starter.gen_hook.adding_bullet(vim.g.have_nerd_font and '\u{f105} ' or '▎ '),
+    starter.gen_hook.aligning('center', 'center'),
+  },
+}
 
 -- ... and there is more!
 --  Check out: https://github.com/nvim-mini/mini.nvim
