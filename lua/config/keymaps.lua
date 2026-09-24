c26fa17524ffb7f84e9c63f986ddab49a5ee13c3
hanyue1014 <wongxuanbin@gmail.com>
2026-09-24 10:52:12 +0800

    Add commenting keymaps
    <leader>lc to comment

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -45,6 +45,11 @@ end, { expr = true, desc = 'Delete next word' })
 vim.keymap.set('v', '>', '>gv', { desc = 'Indent and keep selection' })
 vim.keymap.set('v', '<', '<gv', { desc = 'Outdent and keep selection' })
 
+-- toggle comments with Neovim's built in `gc`, under the [L]anguage group.
+-- `remap` is needed because `gc`/`gcc` are mappings themselves, not builtins
+vim.keymap.set('n', '<leader>lc', 'gcc', { remap = true, desc = '[L]anguage: [C]omment line' })
+vim.keymap.set('x', '<leader>lc', 'gc', { remap = true, desc = '[L]anguage: [C]omment selection' })
+
 -- visual: move the selection (thank you theprimeagen)
 vim.keymap.set('v', 'J', ":m '>+1<cr>gv=gv", { desc = 'Move selection down' })
 vim.keymap.set('v', 'K', ":m '<-2<cr>gv=gv", { desc = 'Move selection up' })
